3a655dd7980a8ed471a3d31f6eb6bdf4936da3cb
HomosCristian <146970235+HomosCristian@users.noreply.github.com>
2024-03-05 13:10:18 +0200

    Add files via upload

---
+++ Seminar/Seminar 3.5/Seminar 3.5/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Seminar_3._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAfisare = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAfisare
+            // 
+            this.btnAfisare.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAfisare.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAfisare.Location = new System.Drawing.Point(214, 209);
+            this.btnAfisare.Name = "btnAfisare";
+            this.btnAfisare.Size = new System.Drawing.Size(142, 54);
+            this.btnAfisare.TabIndex = 1;
+            this.btnAfisare.Text = "Afiseaza";
+            this.btnAfisare.UseVisualStyleBackColor = false;
+            this.btnAfisare.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Te rog frumos introdu textul dorit:";
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(214, 121);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(149, 54);
+            this.btnTransfer.TabIndex = 4;
+            this.btnTransfer.Text = "Transfer";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAfisare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAfisare;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTransfer;
+    }
+}
+
---
+++ Seminar/Seminar 3.5/Seminar 3.5/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar_3._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DialogResult rezultat = MessageBox.Show("Text afisat", "Fereastra mesaj", MessageBoxButtons.OKCancel, MessageBoxIcon.Stop);
+            
+            if (rezultat == DialogResult.Cancel)
+            {
+                MessageBox.Show("Raspuns gresit!");
+            }
+
+        }
+    }
+}
