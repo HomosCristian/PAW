17ab4ec42b8fa8d06ad3d6320db40c43d2f1408e
HomosCristian <146970235+HomosCristian@users.noreply.github.com>
2024-03-12 13:16:50 +0200

    Add files via upload

---
+++ Seminar/Seminar4/Seminar4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Seminar4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMedici = new System.Windows.Forms.ListBox();
+            this.tbMedic = new System.Windows.Forms.TextBox();
+            this.lvMedici = new System.Windows.Forms.ListView();
+            this.Nume = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CNP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Specializare = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.AnNastere = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.AnAbsolvire = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lbMedici
+            // 
+            this.lbMedici.FormattingEnabled = true;
+            this.lbMedici.ItemHeight = 16;
+            this.lbMedici.Items.AddRange(new object[] {
+            "Medic Ionescu",
+            "Medic Popescu"});
+            this.lbMedici.Location = new System.Drawing.Point(12, 290);
+            this.lbMedici.Name = "lbMedici";
+            this.lbMedici.Size = new System.Drawing.Size(277, 132);
+            this.lbMedici.TabIndex = 0;
+            this.lbMedici.SelectedIndexChanged += new System.EventHandler(this.lbMedici_SelectedIndexChanged);
+            // 
+            // tbMedic
+            // 
+            this.tbMedic.Location = new System.Drawing.Point(395, 290);
+            this.tbMedic.Name = "tbMedic";
+            this.tbMedic.Size = new System.Drawing.Size(305, 22);
+            this.tbMedic.TabIndex = 1;
+            // 
+            // lvMedici
+            // 
+            this.lvMedici.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nume,
+            this.CNP,
+            this.AnAbsolvire,
+            this.Specializare,
+            this.AnNastere});
+            this.lvMedici.FullRowSelect = true;
+            this.lvMedici.HideSelection = false;
+            this.lvMedici.Location = new System.Drawing.Point(12, 23);
+            this.lvMedici.Name = "lvMedici";
+            this.lvMedici.Size = new System.Drawing.Size(776, 126);
+            this.lvMedici.TabIndex = 2;
+            this.lvMedici.UseCompatibleStateImageBehavior = false;
+            this.lvMedici.View = System.Windows.Forms.View.Details;
+            this.lvMedici.SelectedIndexChanged += new System.EventHandler(this.lvMedici_SelectedIndexChanged);
+            // 
+            // Nume
+            // 
+            this.Nume.Text = "Nume";
+            this.Nume.Width = 154;
+            // 
+            // CNP
+            // 
+            this.CNP.Text = "CNP";
+            // 
+            // Specializare
+            // 
+            this.Specializare.DisplayIndex = 2;
+            this.Specializare.Text = "Specializare";
+            // 
+            // AnNastere
+            // 
+            this.AnNastere.DisplayIndex = 3;
+            this.AnNastere.Text = "An Nastere";
+            // 
+            // AnAbsolvire
+            // 
+            this.AnAbsolvire.DisplayIndex = 4;
+            this.AnAbsolvire.Text = "An Absolvire";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvMedici);
+            this.Controls.Add(this.tbMedic);
+            this.Controls.Add(this.lbMedici);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMedici;
+        private System.Windows.Forms.TextBox tbMedic;
+        private System.Windows.Forms.ListView lvMedici;
+        private System.Windows.Forms.ColumnHeader Nume;
+        private System.Windows.Forms.ColumnHeader CNP;
+        private System.Windows.Forms.ColumnHeader AnAbsolvire;
+        private System.Windows.Forms.ColumnHeader Specializare;
+        private System.Windows.Forms.ColumnHeader AnNastere;
+    }
+}
+
---
+++ Seminar/Seminar4/Seminar4/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar4
+{
+    public partial class Form1 : Form
+    {
+        Medic m1, m2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m1 = new Medic("58392171982", "Marian", 2020, "Chirurgie Plastica", new DateTime(1980, 05, 25));
+            m2 = new Medic("65858393958", "Ana", 2015, "Neurologie", new DateTime(1978, 08, 15));
+
+            lbMedici.Items.Add(m1.Nume);
+            lbMedici.Items.Add(m2.Nume);
+
+            ListViewItem lv1 = new ListViewItem(m1.Nume);
+            lv1.SubItems.Add(m1.Cnp);
+            lv1.SubItems.Add(m1.AnAbsolvire.ToString());
+            lv1.SubItems.Add(m1.Specializare);
+            lv1.SubItems.Add(m1.DataNastere.ToString());
+            
+            lv1.Tag = m1;   //asociem linia cu medicul 1
+            lvMedici.Items.Add(lv1);
+
+            ListViewItem lv2 = new ListViewItem(m2.Nume);
+            lv2.SubItems.Add(m2.Cnp);
+            lv2.SubItems.Add(m2.AnAbsolvire.ToString());
+            lv2.SubItems.Add(m2.Specializare);
+            lv2.SubItems.Add(m2.DataNastere.ToString());
+            lv2.Tag = m2;
+            lvMedici.Items.Add(lv2);
+
+
+        }
+
+        private void lvMedici_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lvMedici.SelectedItems.Count > 0)
+            {
+                tbMedic.Text = "";
+                //foreach(ListViewItem lv in lvMedici.SelectedItems)
+                ListViewItem lv = lvMedici.SelectedItems[0];
+                tbMedic.Text += lv.Text + ", " + lv.SubItems[1].Text + ", " + lv.SubItems[2].Text + ", " + lv.SubItems[3].Text + ", "
+                                + lv.SubItems[4].Text;
+                //Medic ml = (Medic) lv.Tag;
+                //tbMedic.Text = ml.Nume + " " + ml.Cnp;
+            }
+        }
+
+        private void lbMedici_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lbMedici.SelectedItems!=null)
+            tbMedic.Text = lbMedici.SelectedItem.ToString();
+        }
+    }
+}
